4b342f94b3c95a833066c6e53f9fc616e36350f7
JoacoPruebas <DestroyerJoaco@gmail.com>
2025-05-09 20:30:42 -0300

    adaptacion server para app de productos

--- app-productos/server.js
+++ app-productos/server.js
@@ -1,31 +1,31 @@
 // server.js
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 const db = require('./db');
 
 app.use(express.json());
 app.use(express.static(__dirname));
 
-app.get('/personas', (req, res) => {
+app.get('/productos', (req, res) => {
   res.json(db.getAll());
 });
 
-app.post('/personas', (req, res) => {
+app.post('/productos', (req, res) => {
   db.add(req.body);
-  res.status(201).json({ mensaje: 'Persona agregada' });
+  res.status(201).json({ mensaje: 'Producto agregado' });
 });
 
-app.delete('/personas/:nombre', (req, res) => {
+app.delete('/productos/:nombre', (req, res) => {
   db.remove(req.params.nombre);
-  res.json({ mensaje: 'Persona eliminada' });
+  res.json({ mensaje: 'Producto eliminado' });
 });
 
-app.put('/personas/:nombreOriginal', (req, res) => {
+app.put('/productos/:nombreOriginal', (req, res) => {
   db.update(req.params.nombreOriginal, req.body);
-  res.json({ mensaje: 'Persona actualizada' });
+  res.json({ mensaje: 'Producto actualizado' });
 });
 
 app.listen(port, () => {
-  console.log(`Servidor de personas corriendo en http://localhost:${port}`);
+  console.log(`Servidor de productos corriendo en http://localhost:${port}`);
 });
